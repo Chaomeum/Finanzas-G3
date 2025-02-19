3931e80ee5e393e5931bbcdef56d90f506a24a4e
Jhon Galvez <jhon.galvezchambi@gmail.com>
2025-02-19 00:20:05 -0500

    Update settings and ignore database file

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '../FRONT/')],
+        'DIRS': [os.path.join(BASE_DIR, '../frontend/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
