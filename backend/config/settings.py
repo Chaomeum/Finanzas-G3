075054083444f3c12ac9743937ab0e4fda3505cf
Jhon Galvez <jhon.galvezchambi@gmail.com>
2025-03-01 15:09:15 -0500

    Respaldo de cambios en backend antes de actualizar frontend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Propio
-    'apps.usuarios',    
+    'apps.usuarios',
+    'apps.core',
 ]
 
 MIDDLEWARE = [
